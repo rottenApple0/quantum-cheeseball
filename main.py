39b0bdfcc9fde36b117018df7894c25a4b72eaa3
Coop <ominoussylph@gmail.com>
2025-04-03 12:39:41 -0500

    added runnable class

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
-# Hi godric!!!
+class QuantumCheeseball:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Quantum Cheeseball")
+        
+if __name__ == "__main__":
+    qc = QuantumCheeseball()
+    qc.run()
+
